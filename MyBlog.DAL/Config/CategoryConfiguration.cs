5cd02d2d8a1a857fffaf4cf33134860d6b6f831e
nilay ergul <nilayergul21@gmail.com>
2019-09-11 14:20:49 +0300

    DAL Çalışma

---
+++ MyBlog.DAL/Config/CategoryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.DAL.Config
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(x => x.CategoryName)
+            .IsRequired()
+            .HasMaxLength(256);
+
+            builder.Property(x => x.Description)
+                .IsRequired(false);
+
+
+        }
+    }
+}
